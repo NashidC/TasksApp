44fa22d645e59f662b66023d36d7080dacf1b9d7
NashidC <nash.chowdhury@gmail.com>
2024-12-19 20:54:45 -0500

    initial commit

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -9,15 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
     var tasks  = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tasks = []
+        
         //Get all current saved tasks
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
     }
 
+    @IBAction func didTapAdd() {
+        
+    }
 
 }
 
@@ -25,19 +34,22 @@ extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
 }
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Number of rows in section: \(tasks.count)")
         return tasks.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         cell.textLabel?.text = tasks[indexPath.row]
-        print("Success")
-        return cell
         
+        return cell
+       
+        //video 10:40
     }
 }
+
+
